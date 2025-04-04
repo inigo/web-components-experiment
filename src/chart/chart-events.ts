89f95a5696fe128806b9e7264332b1a3232a2a43
Inigo Surguy <inigo.surguy@gmail.com>
2025-04-04 13:10:36 +0100

    Configure the data-event-mediator via an attribute, primarily to make it more obvious

--- src/chart/chart-events.ts
+++ src/chart/chart-events.ts
@@ -1,31 +1,43 @@
 import {SlSelectEvent} from "@shoelace-style/shoelace";
 
-export type DataChartTypeChangedEvent = CustomEvent<{ chartType: string }>;
-
+/**
+ * Decouples the chart component from the components changing its values, by
+ * converting and re-raises the relevant events.
+ */
 export class DataEventMediator extends HTMLElement {
-    constructor() {
-        super();
+    private chartTypePurpose: string = 'chartType';
 
-        document.addEventListener('sl-select', (event: Event) => {
-            const selectEvent = (event as SlSelectEvent);
+    private chartTypeMediator = (event: Event) => {
+        const selectEvent = (event as SlSelectEvent);
 
-            const purpose = ((selectEvent.target as HTMLElement)
-                .closest("[data-purpose]") as HTMLElement)
-                ?.dataset.purpose;
-            const selectedItem = selectEvent.detail.item.value;
+        const purpose = ((selectEvent.target as HTMLElement)
+            .closest("[data-purpose]") as HTMLElement)
+            ?.dataset.purpose;
+        const selectedItem = selectEvent.detail.item.value;
 
-            if (purpose === "chartType") {
-                console.debug(`Launched new event`);
+        if (purpose === this.chartTypePurpose) {
+            console.debug(`Raised new chart type changed event with value '${selectedItem}'`);
+            const event: DataChartTypeChangedEvent = new CustomEvent('data-chartType-changed', {
+                bubbles: true,
+                detail: { chartType: selectedItem }
+            });
+            document.dispatchEvent(event);
+        }
+    };
 
-                const event: DataChartTypeChangedEvent = new CustomEvent('data-chartType-changed', {
-                    bubbles: true,
-                    detail: { chartType: selectedItem }
-                });
-                document.dispatchEvent(event);
-            }
-        });
+    // noinspection JSUnusedGlobalSymbols
+    connectedCallback() {
+        this.chartTypePurpose = this.getAttribute('chartTypePurpose') ?? this.chartTypePurpose;
+        document.addEventListener('sl-select', this.chartTypeMediator);
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    disconnectedCallback() {
+        document.removeEventListener('sl-select', this.chartTypeMediator);
     }
 }
+
+export type DataChartTypeChangedEvent = CustomEvent<{ chartType: string }>;
 
 export function registerDataEventMediator(): void {
     if (!customElements.get('data-event-mediator')) {
