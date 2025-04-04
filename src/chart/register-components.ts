dc72f09af804b6c73f4fcaf9281db265b4cab79c
Inigo Surguy <inigo.surguy@gmail.com>
2025-04-04 16:40:15 +0100

    Added note on registering Lit elements

--- src/chart/register-components.ts
+++ src/chart/register-components.ts
@@ -8,4 +8,6 @@ export function registerComponents(): void {
     if (!customElements.get('data-history-manager')) {
         customElements.define('data-history-manager', DataHistoryManager);
     }
+    // The Lit elements like data-chart do not need to be registered separately, because @customElement does it
+    // but that can't be applied to non-Lit elements.
 }
