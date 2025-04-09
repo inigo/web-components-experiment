c1317c9d2803a689f6c9b8e2aab05e8e2cb1a26d
Inigo Surguy <inigo.surguy@gmail.com>
2025-04-09 17:53:49 +0100

    Remove another reference to slim-select

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,6 @@ export default defineConfig({
                     shoelace: ['@shoelace-style/shoelace/dist/shoelace.js'],
                     vendor: [
                         'vanilla-cookieconsent',
-                        'slim-select',
                     ]
                 },
             },
