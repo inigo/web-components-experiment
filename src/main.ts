144ec202cd23311bdc262789b3146158e8aec3ca
Inigo Surguy <inigo.surguy@gmail.com>
2025-04-09 16:07:52 +0100

    Remove the unused shoelace-based chart type dropdown

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,6 @@ import 'sortable-tablesort/dist/sortable.min.js'
 import 'sortable-tablesort/dist/sortable.a11y.min.js'
 
 import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
-import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
-import '@shoelace-style/shoelace/dist/components/menu/menu.js';
-import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
